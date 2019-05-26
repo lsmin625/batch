40c3f942e47adb8a1b2082c3dfd4f92c58d0473b
이상민(lenovo) <lsmin625@gmail.com>
2019-05-26 23:55:51 +0900

    lenovo에서 실행파일 추가

---
+++ leno-dev.bat
@@ -0,0 +1,1 @@
+java -jar -Dspring.profiles.active=dev D:\workspace\github-batch\target\batch-job-0.0.1-SNAPSHOT.jar
---
+++ leno-local.bat
@@ -0,0 +1,1 @@
+java -jar -Dspring.profiles.active=local D:\workspace\github-batch\target\batch-job-0.0.1-SNAPSHOT.jar
